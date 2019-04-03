787dfb02b727797bd56ab294a276d7b66ecb2253
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-03 18:20:28 +0300

    updated eslint config. Fixed eslint errors.

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -6,8 +6,8 @@
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
  */
 
-function applyCustomUnshift () {
-  [].__proto__.unshift2 = function () {
+function applyCustomUnshift() {
+  [].__proto__.unshift2 = function() {
     // write code here
   };
 }
