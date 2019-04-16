bcbd75c2defe01fd75659a30b06be3e01da41e91
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-16 23:05:51 +0300

    Yevhen Kryvdiuk

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,8 +5,18 @@
  *
  */
 function applyCustomUnshift() {
-  [].__proto__.unshift2 = function() {
-    // write code here
+  [].__proto__.unshift2 = function(...args) {
+    if (args.length === 0) {
+      return this.length;
+    }
+    const thisArray = [...this];
+    args.forEach((value, index) => {
+      this[index] = value;
+    });
+    thisArray.forEach((value, index) => {
+      this[args.length + index] = value;
+    });
+    return this.length;
   };
 }
 
