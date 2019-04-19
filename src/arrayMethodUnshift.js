2985bfe416846c8f2bc465c67205a07c2c91f55c
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-19 08:08:27 +0300

    solution for array method unshift by @laverka

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,18 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    if (!elements.length) return this.length;
+    const objectLength = this.length;
+    let argumentsLength = elements.length;
+    let newArrayLength = objectLength + argumentsLength;
+    this.length += elements.length;
+    for (let i = newArrayLength - 1; i >= 0; i--) {
+      this[i] = this[i - argumentsLength];
+      if (i <= argumentsLength - 1) {
+        this[i] = elements[i];
+      }
+    }
+    return this.length;
   };
 }
 
