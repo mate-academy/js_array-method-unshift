f29de7e21d722995b8171dd1cdc7aed85e2d994c
valeriitovstyk <valeriitovstyk@gmail.com>
2019-04-19 08:32:04 +0300

    changes let-> const for variables

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -7,8 +7,8 @@ function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
     if (!elements.length) return this.length;
     const objectLength = this.length;
-    let argumentsLength = elements.length;
-    let newArrayLength = objectLength + argumentsLength;
+    const argumentsLength = elements.length;
+    const newArrayLength = objectLength + argumentsLength;
     this.length += elements.length;
     for (let i = newArrayLength - 1; i >= 0; i--) {
       this[i] = this[i - argumentsLength];
