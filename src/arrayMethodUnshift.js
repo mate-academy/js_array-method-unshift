560a4f3e3ab018a94a834699a639a8972942c30c
OleksandrHlybchenko <logista@meta.ua>
2019-06-18 20:54:29 +0300

    for review

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,14 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    for (let i = this.length - 1; i >= 0; i--) {
+      this[elements.length + i] = this[i];
+    }
+    for (let j = 0; j < elements.length; j++) {
+      this[j] = elements[j];
+    }
+
+    return this.length;
   };
 }
 
