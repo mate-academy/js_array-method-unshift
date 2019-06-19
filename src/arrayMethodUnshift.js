310a4075b215f776f41b92b4c5c57ae96c436d39
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-19 16:50:56 +0300

    unshift

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const values = [];
+    for (let i = 0; i < elements.length; i++) {
+      values[i] = elements[i];
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      values[values.length] = this[i];
+    }
+
+    for (let i = 0; i < values.length; i++) {
+      this[i] = values[i];
+    }
+
+    return this.length;
   };
 }
 
