f11c7fa32215122e048e48e8810bed5ba6258735
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-19 17:31:53 +0300

    fix

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -4,18 +4,13 @@
  * Implement method unshift
  */
 function applyCustomUnshift() {
-  [].__proto__.unshift2 = function(...elements) {
-    const values = [];
-    for (let i = 0; i < elements.length; i++) {
-      values[i] = elements[i];
-    }
-
+  [].__proto__.unshift2 = function(...elms) {
     for (let i = 0; i < this.length; i++) {
-      values[values.length] = this[i];
+      elms[elms.length] = this[i];
     }
 
-    for (let i = 0; i < values.length; i++) {
-      this[i] = values[i];
+    for (let i = 0; i < elms.length; i++) {
+      this[i] = elms[i];
     }
 
     return this.length;
