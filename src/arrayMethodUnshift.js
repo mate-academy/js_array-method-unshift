d74ce2d5d288a0eca8f6eb36a543b42d9390df29
neitiris <veliantos@gmail.com>
2019-06-20 16:50:31 +0300

    solution commit

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -6,6 +6,18 @@
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
     // write code here
+    if (elements) {
+      this.length = this.length + elements.length;
+      for (let i = this.length - 1; i > elements.length - 1; i--) {
+        this[i] = this[i - elements.length];
+      }
+      for (let i = 0; i < elements.length; i++) {
+        this[i] = elements[i];
+      }
+      return this.length;
+    } else {
+      return undefined;
+    }
   };
 }
 
