c571c1babc36bd3431e28a70ab136a32be9177d9
Olga Lyubicheva <forjobslov@gmail.com>
2019-06-21 13:38:11 +0300

    solution

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,17 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    if (arguments.length === 0) {
+      return this.length;
+    }
+
+    const newThis = [...arguments, ...this];
+
+    for (let i = 0; i < newThis.length; i++) {
+      this[i] = newThis[i];
+    }
+
+    return this.length;
   };
 }
 
