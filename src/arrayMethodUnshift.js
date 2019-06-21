9be3976285be8766d854d7be1cc856328ccd8677
Пользователь <Sviatoslv>
2019-06-21 12:32:39 +0300

    New legth of this arr logic chenged

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,9 +5,9 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    for (let elemIndex = elements.length - 1; elemIndex >= 0; elemIndex--) {
-      this.length = this.length + 1;
+    this.length = this.length + [...elements].length;
 
+    for (let elemIndex = elements.length - 1; elemIndex >= 0; elemIndex--) {
       for (let i = this.length - 1; i > 0; i--) {
         this[i] = this[i - 1];
       }
