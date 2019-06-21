9a1ffc515da219de471bceef4c23dc14788d415a
Olga Lyubicheva <forjobslov@gmail.com>
2019-06-21 18:15:36 +0300

    change

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -9,7 +9,16 @@ function applyCustomUnshift() {
       return this.length;
     }
 
-    const newThis = [...arguments, ...this];
+    const newThis = [];
+    const lengthOfArgum = arguments.length;
+
+    for (let i = 0; i < lengthOfArgum; i++) {
+      newThis[i] = arguments[i];
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      newThis[i + lengthOfArgum] = this[i];
+    }
 
     for (let i = 0; i < newThis.length; i++) {
       this[i] = newThis[i];
