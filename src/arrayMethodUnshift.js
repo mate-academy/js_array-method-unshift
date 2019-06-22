7682cbffd4dbfd9d78fbc85df3b5fe30543911c9
scandal0us <d.l.lutsak@gmail.com>
2019-06-22 12:55:55 +0300

    Solution

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -6,6 +6,20 @@
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
     // write code here
+    const elementsToAdd = [...elements];
+
+    if (elementsToAdd.length === 0) {
+      return this.length;
+    } else {
+      for (let i = this.length + elementsToAdd.length - 1; i >= 0; i--) {
+        if (i >= elementsToAdd.length) {
+          this[i] = this[i - elementsToAdd.length];
+        } else {
+          this[i] = elementsToAdd[i];
+        };
+      }
+      return this.length;
+    };
   };
 }
 
