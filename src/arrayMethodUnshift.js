6aaa020d863336f1b505ab35a848cdc74b3aca56
yevhen-sahach <antitop04@gmail.com>
2019-08-16 18:29:09 +0300

    first commit

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,16 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    for (let i = elements.length - 1; i >= 0; i--) {
+      this.length++;
+      for (let j = this.length - 1; j >= 0; j--) {
+        this[j] = this[j - 1];
+      }
+
+      this[0] = elements[i];
+    }
+
+    return this.length;
   };
 }
 
