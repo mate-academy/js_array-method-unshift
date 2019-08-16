f384d9a035d07570cf26bca7f215694408f94240
KseniiaMazan <ksusamazan@gmail.com>
2019-08-16 17:32:36 +0300

    fixed comments

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -7,10 +7,10 @@ function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
     this.reverse();
 
-    for (let i = elements.length; i > 0;) {
-      this[this.length] = elements[i - 1];
-      i = i - 1;
+    for (let i = elements.length - 1; i >= 0; i--) {
+      this[this.length] = elements[i];
     }
+
     this.reverse();
     return this.length;
   };
