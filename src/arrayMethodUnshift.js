1742e8516ce479d137925ad4722755f7f3bc9cc3
Viktor Pastushenko <pastushenko.vik@gmail.com>
2019-08-16 17:15:47 +0300

    fix empty line

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,15 +5,12 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    if (this.length === 0) {
-      return undefined;
-    }
-
     for (let i = elements.length - 1; i >= 0; i--) {
       ++this.length;
       for (let j = this.length - 1; j >= 0; j--) {
         this[j] = this[j - 1];
       }
+
       this[0] = elements[i];
     }
 
