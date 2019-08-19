5a1af2e201b1accda75c8d6d233b872539ea0f96
youvovk <wer3wollf@gmail.com>
2019-08-19 13:17:07 +0300

    delete unnecassery rules

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,17 +5,11 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    if (elements.length === 0) {
-      return this.length;
-    }
-
     const copyArr = [...elements];
 
     for (let i = 0; i < this.length; i++) {
       copyArr[copyArr.length] = this[i];
     }
-
-    this.length = 0;
 
     for (let i = 0; i < copyArr.length; i++) {
       this[i] = copyArr[i];
