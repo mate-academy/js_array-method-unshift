690c3589cc8c766952a26855ce0df84230c65a4d
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-08-21 10:11:55 +0300

    'Solution'

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,16 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const arrLength = elements.length;
+    for (let i = this.length - 1; i >= 0; i--) {
+      this[i + arrLength] = this[i];
+    }
+
+    for (let i = 0; i < arrLength; i++) {
+      this[i] = elements[i];
+    }
+
+    return this.length;
   };
 }
 
