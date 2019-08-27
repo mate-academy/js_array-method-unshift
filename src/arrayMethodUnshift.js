57b3fdad39c4bde6f2e87e41fbea937878ba6ac4
Rafler <amuronyk@gmail.com>
2019-08-27 14:43:15 +0300

    spacing

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -6,9 +6,11 @@
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
     const arr = [...elements, ...this];
+
     for (let i = 0; i < arr.length; i++) {
       this[i] = arr[i];
     }
+
     return this.length;
   };
 }
