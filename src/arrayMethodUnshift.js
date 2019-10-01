d3f813769e41dab369424b99302202a7552e7d1f
VadimK <djvadya16@gmail.com>
2019-10-01 17:40:31 +0300

    done

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,23 +5,19 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    if (!this.length) {
-      return undefined;
-    } else {
-      const startArray = [ ...this ];
-      let position = 0;
+    const startArray = [ ...this ];
+    let position = 0;
 
-      for (const key of elements) {
-        this[position] = key;
-        position++;
-      }
-      for (let i = 0; i < startArray.length; i++) {
-        this[position] = startArray[i];
-        position++;
-      }
-
-      return this.length;
+    for (const key of elements) {
+      this[position] = key;
+      position++;
     }
+    for (let i = 0; i < startArray.length; i++) {
+      this[position] = startArray[i];
+      position++;
+    }
+
+    return this.length;
   };
 }
 
