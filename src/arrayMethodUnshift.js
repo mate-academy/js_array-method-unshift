fc76eb7f9e6ac98c66cdd24a77dee8bf4423983b
Vlad Mishchenko <vladislav.a.mishchenko@gmail.com>
2019-11-19 20:30:58 +0200

    develop

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,24 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const clone = [];
+    for (const key of this) {
+      clone[key] = this[key];
+    }
+    this.length = 0;
+    let i;
+    for (i = 0; i < elements.length; i++) {
+      this[i] = elements[i];
+    }
+
+    let j = 0;
+    i = this.length;
+    while (j < clone.length) {
+      this[i] = clone[j];
+      i++;
+      j++;
+    }
+    return this.length;
   };
 }
 
