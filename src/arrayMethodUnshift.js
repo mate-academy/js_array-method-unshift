d06417bd211aaf41e0627162ec4dc75519767144
Oles Marola <oles_pw@ukr.net>
2019-11-19 15:36:17 +0200

    unshift

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    if (this.length < 1) {
+      return undefined;
+    }
+    const afterShift = [];
+    for (let i = 0; i < elements.length; i++) {
+      afterShift[i] = elements[i];
+    }
+    for (let i = 0; i < this.length; i++) {
+      afterShift[afterShift.length] = this[i];
+    }
+    for (let i = 0; i < afterShift.length; i++) {
+      this[i] = afterShift[i];
+    }
+    return this.length;
   };
 }
 
