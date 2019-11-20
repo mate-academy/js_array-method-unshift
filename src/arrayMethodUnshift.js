a2e890fa0dc540e39d0a4eebe37a983ecfd7d306
Inna <inna.tereshchenko1@gmail.com>
2019-11-20 14:22:12 +0200

    solution

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,16 +5,12 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    const k = [...this];
+    for (let i = 0; i < this.length; i++) {
+      elements[elements.length] = this[i];
+    }
 
     for (let i = 0; i < elements.length; i++) {
       this[i] = elements[i];
-    }
-
-    this.length = elements.length;
-
-    for (let i = 0; i < k.length; i++) {
-      this[this.length] = k[i];
     }
 
     return this.length;
