383703ea2510ca1527ee426741e24453c0c1b5a9
natya2203 <nastya_2203@ukr.net>
2019-11-20 14:10:52 +0200

    solition

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const a = this.length + arguments.length;
+    const arr = [];
+    for (let i = 0; i < arguments.length; i++) {
+      arr[i] = arguments[i];
+    }
+    for (let i = arr.length, j = 0; j < this.length; i++, j++) {
+      arr[i] = this[j];
+    }
+
+    for (let i = 0; i < a; i++) {
+      this[i] = arr[i];
+    }
+
+    return this.length;
   };
 }
 
