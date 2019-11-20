89ecb9157493b63ef9b6f7d2e885dba4ff9f1dc2
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-20 13:41:31 +0200

    Aidinov Alexii

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,21 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    this.length += elements.length;
+
+    for (let i = this.length - 1; i >= 0; i--) {
+      this[i] = this[i - elements.length];
+    }
+
+    for (let i = 0; i < elements.length; i++) {
+      this[i] = elements[i];
+    }
+
+    return this.length;
   };
 }
 
