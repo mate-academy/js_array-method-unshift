8ebf694bce1ccc03972341f4456a8f760f465939
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-24 20:08:09 +0200

    done

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    this.reverse();
+
+    for (let i = elements.length - 1; i >= 0; i--) {
+      this.length += 1;
+      this[this.length - 1] = elements[i];
+    }
+
+    this.reverse();
+
+    return this.length;
   };
 }
 
