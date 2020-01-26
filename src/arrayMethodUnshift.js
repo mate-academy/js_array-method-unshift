2f252b4e29d5b4136bd2e4ba477d02c5fe162cf6
Senya Volkov <formobileprima@gmail.com>
2020-01-27 01:12:50 +0200

    solution

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,17 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const mainArr = Array.from(this);
+
+    for (let i = (mainArr.length - 1); i >= 0; i--) {
+      this[i + elements.length] = this[i];
+    }
+
+    for (let k = 0; k < elements.length; k++) {
+      this[k] = elements[k];
+    }
+
+    return this.length;
   };
 }
 
