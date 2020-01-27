2557d9b3c2329410d24d2f1da0ab0241124d63cb
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-01-27 17:49:29 +0200

    changed name variable and changed cicle

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -7,15 +7,15 @@ function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
     if (!elements.length) {
       return this.length;
-    } else {
-      const unshiftArr = [...elements, ...this];
-
-      for (let i = 0; i < unshiftArr.length; i++) {
-        this[i] = unshiftArr[i];
-      }
-
-      return this.length;
     }
+
+    const array = [...elements, ...this];
+
+    for (let i = 0; i < array.length; i++) {
+      this[i] = array[i];
+    }
+
+    return this.length;
   };
 }
 
