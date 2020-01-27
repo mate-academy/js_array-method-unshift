90a9374ecb87843f5b9ed4ef17e8364117aa1fb1
Olexander <kravchenko.sania@gmail.com>
2020-01-27 22:55:41 +0200

    second attempt

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,11 +5,11 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    for (let i = this.length + elements.length - 1; i >= elements.length; --i) {
+    for (let i = this.length + elements.length - 1; i >= elements.length; i--) {
       this[i] = this[i - elements.length];
     }
 
-    for (let i = 0; i < elements.length; ++i) {
+    for (let i = 0; i < elements.length; i++) {
       this[i] = elements[i];
     }
 
