2842c29b6d9c1af0b5bd3a4131ffb28fbd016f03
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-01-27 15:22:42 +0200

    Updated

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,8 +5,10 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    for (const element of elements.reverse()) {
-      this.length = this.length + 1;
+    const reverseElements = elements.reverse();
+
+    for (const element of reverseElements) {
+      this.length++;
 
       for (let i = this.length - 1; i > 0; i--) {
         this[i] = this[i - 1];
