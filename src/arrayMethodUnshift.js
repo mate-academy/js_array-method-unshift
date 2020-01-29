808afc769277b15de2c343db55816a27bbaf91a7
ded-git <sakarailko@gmail.com>
2020-01-29 16:28:38 +0200

    did it

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const quontity = [...elements].length;
+
+    for (let i = this.length - 1; i >= 0; i--) {
+      this[i + quontity] = this[i];
+    }
+
+    let count = 0;
+
+    for (let q = 0; q < quontity; q++) {
+      this[count] = arguments[q];
+      count++;
+    }
+
+    return this.length;
   };
 }
 
