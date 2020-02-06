27cb0d4fbf7265e10fed8377c055fa025f57951c
ronyshchenko <roma.onyshchenko@gmail.com>
2020-02-06 13:25:25 +0200

    6_2_20_two

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -3,9 +3,16 @@
 /**
  * Implement method unshift
  */
-function applyCustomUnshift() {
-  [].__proto__.unshift2 = function(...elements) {
-    // write code here
+function applyCustomUnshift(...elements) {
+  [].__proto__.unshift2 = function() {
+    for (let i = elements.length - 1; i >= 0; i--) {
+      for (let j = this.length; j > 0; j--) {
+        this[j] = this[j - 1];
+      }
+      this[0] = elements[i];
+    }
+
+    return this.length;
   };
 }
 
