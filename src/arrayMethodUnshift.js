be360e725ceb9fb0398d7cd1bd6f9a2e8e2cad03
Vitalii Korol <vitaliikorol@github.com>
2020-04-01 18:25:34 +0300

    fix

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,19 +5,15 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
+    for (let i = 0; i < this.length; i++) {
+      elements[elements.length] = this[i];
+    }
+
     for (let i = 0; i < elements.length; i++) {
-      for (let j = 0; j < this.length; j++) {
-        this[this.length - j] = this[this.length - j - 1];
-      }
+      this[i] = elements[i];
     }
 
-    for (let k = 0; k < elements.length; k++) {
-      this[k] = elements[k];
-    }
-
-    const quantity = this.length;
-
-    return quantity;
+    return this.length;
   };
 }
 
