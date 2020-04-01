2a76dfcd5f5a486cda45c137b314ef6f15e960c4
WincherZ <wincher2016@gmail.com>
2020-04-01 23:31:55 +0300

    some changes

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const arr = this;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+      arr[i + elements.length] = arr[i];
+    };
+
+    let counter = 0;
+
+    for (const item of elements) {
+      arr[counter] = item;
+      counter++;
+    }
+
+    return arr.length;
   };
 }
 
