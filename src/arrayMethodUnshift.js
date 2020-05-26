1ed2401626afd8b778c6bb44f15281e899e9842e
solaryasha <solaryasha@gmail.com>
2020-05-26 18:42:46 +0300

    Solution

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -6,6 +6,21 @@
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
     // write code here
+
+    const newLength = elements.length + this.length;
+
+    // start from the end of the concatanated array
+    for (let i = newLength - 1; i >= 0; i--) {
+    // check if the script should shift values to right
+      if (i >= elements.length) {
+        this[i] = this[i - elements.length];
+      } else {
+        // if no values to shift, add new elements to the array
+        this[i] = elements[i];
+      }
+    }
+
+    return newLength;
   };
 }
 
