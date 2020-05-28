b7ee63e99f2d9f7853a9ccc8f6d4731fe8e69adb
Glib Rulev <velzepooz@gmail.com>
2020-05-28 16:31:51 +0300

    Solution

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,20 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const temp = [...this];
+    let index = 0;
+
+    const allLength = this.length + elements.length;
+
+    for (let i = 0; i < allLength; i++) {
+      if (i < elements.length) {
+        this[i] = elements[i];
+      } else {
+        this[i] = temp[index++];
+      }
+    }
+
+    return this.length;
   };
 }
 
