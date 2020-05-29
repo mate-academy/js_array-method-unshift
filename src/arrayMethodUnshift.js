5b739ded0ca6ee2cd4abc11cb717eef912d197cc
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-05-29 21:38:36 +0300

    solution

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,15 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    for (let i = this.length - 1; i >= 0; i--) {
+      this[i + elements.length] = this[i];
+
+      if (i < elements.length) {
+        this[i] = elements[i];
+      }
+    }
+
+    return this.length;
   };
 }
 
