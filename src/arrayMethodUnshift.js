7e08b3f6eb06e6559c988cd447af236bcb4e894f
tyooma <flying.alien.godzilla@gmail.com>
2020-05-30 20:56:50 +0300

    DIY Unshift Method

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,23 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    const newArrLength = this.length + arguments.length;
+
+    const arr = [];
+
+    for (let i = 0; i < this.length; i++) {
+      arr[i] = this[i];
+    }
+
+    for (let i = 0; i < newArrLength; i++) {
+      this[i] = elements[i];
+
+      if (this[i] === undefined) {
+        this[i] = arr[i - arguments.length];
+      }
+    }
+
+    return newArrLength;
   };
 }
 
