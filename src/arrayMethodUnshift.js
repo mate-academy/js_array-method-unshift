f6d19e71cdb52e96526e7bab7abd9dd845621203
DTR <t.terry.rice@gmail.com>
2020-05-30 13:42:37 +0300

    final 1.0

--- src/arrayMethodUnshift.js
+++ src/arrayMethodUnshift.js
@@ -5,7 +5,18 @@
  */
 function applyCustomUnshift() {
   [].__proto__.unshift2 = function(...elements) {
-    // write code here
+    let o = this.length - 1;
+
+    for (let i = this.length + elements.length; i > elements.length; i--) {
+      this[i - 1] = this[o];
+      o--;
+    };
+
+    for (let i = 0; i < elements.length; i++) {
+      this[i] = elements[i];
+    }
+
+    return this.length;
   };
 }
 
